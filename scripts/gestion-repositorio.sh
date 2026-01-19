48025a3f823c90d3c081c4be63f953acd2f4611c
Maalfer <marioalvarezfdz@gmail.com>
2026-01-19 23:20:30 +0100

    update

--- scripts/gestion-repositorio.sh
+++ scripts/gestion-repositorio.sh
@@ -126,7 +126,7 @@ if ! command -v reprepro >/dev/null 2>&1; then
     sudo apt install -y reprepro
 fi
 
-# PySide6/Qt Dependencies (Linux)
+# PySide6/Qt Dependencies
 REQUIRED_LIBS="libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0"
 echo -e "${YELLOW}→ Verificando librerías de sistema para PySide6/Qt...${NC}"
 
